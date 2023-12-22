ffb3189a49f4b2a3e81cd06ae731037b6dbf78cc
dhirajsahh <dhirajmahato2019@gmail.com>
2023-12-22 16:49:46 +0545

    SignUp form UI

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,16 +2,12 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="absolute z-50 px-8 py-2 bg-gradient-to-b from-black">
+    <div className="absolute z-50 md:px-8 md:py-2 md:bg-gradient-to-b from-black">
       <img
-        className="w-44 "
+        className="w-32 md:w-44 "
         src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
         alt="logo"
       />
-
-      <button className="bg-red-700 text-white px-3 py-1 rounded-md ">
-        Sign In
-      </button>
     </div>
   );
 };
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import Headers from "./Header";
 
 const Login = () => {
+  const [isSignInForm, setIsSignForm] = useState(true);
+
+  const toggleSignInForm = () => {
+    setIsSignForm(!isSignInForm);
+  };
   return (
     <div>
       <Headers />
@@ -11,19 +16,35 @@ const Login = () => {
           alt="BackgroundImage"
         />
       </div>
-      <form className=" text-white absolute w-3/12  bg-black my-36 mx-auto right-0 left-0 flex flex-col gap-7 px-8 py-16 rounded-lg bg-opacity-90">
-        <h1 className="font-bold  text-3xl">Sign In</h1>
+      <form className=" text-white absolute w-full h-full md:h-auto md:w-3/12 bg-black md:my-36 mx-auto right-0 left-0 flex flex-col gap-7 px-12 py-16 rounded-lg bg-opacity-90">
+        <h1 className="font-semibold  text-3xl">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
+        {!isSignInForm && (
+          <input
+            type="text"
+            placeholder="Fulll Name"
+            className="p-4 w-full bg-gray-700 rounded-lg focus:outline-none"
+          />
+        )}
         <input
           type="text"
           placeholder="Email Adress"
-          className="p-4 w-full bg-gray-700 rounded-lg"
+          className="p-4 w-full bg-gray-700 rounded-lg focus:outline-none"
         />
         <input
           type="password"
           placeholder="Password"
-          className="p-4 w-full bg-gray-700 rounded-lg"
+          className="p-4 w-full bg-gray-700 rounded-lg focus:outline-none"
         />
-        <button className="p-4 bg-red-600 w-full rounded-lg">Sign In</button>
+        <button className="p-4 bg-red-600 w-full rounded-lg">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </button>
+        <p onClick={toggleSignInForm} className="cursor-pointer">
+          {isSignInForm
+            ? "New to Netflix? Sign Up Now"
+            : "Already a User Sign In"}
+        </p>
       </form>
     </div>
   );
